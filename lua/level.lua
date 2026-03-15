ab1a556d2e325ab2309a1aa6a1e2a9da54fc1660
beelzebold-ii <futurefast125@gmail.com>
2026-03-15 04:35:57 -0500

    fixed up wall drawing; next to make a level editor for further work

--- lua/level.lua
+++ lua/level.lua
@@ -12,6 +12,9 @@ level = {
 	size = Vector2(32,32),
 }
 
+canv_floortile = love.graphics.newCanvas(800,600)
+canv_walls = love.graphics.newCanvas(800,600)
+
 -- initializes the level to a given size, filled with a given tileset
 function level.InitLevel(size,fillset)
 	if getmetatable(size) ~= Vector2Meta then
@@ -47,24 +50,74 @@ function level.DrawLevel(camera)
 			end
 		end
 	
+	love.graphics.setCanvas(canv_floortile)
+	love.graphics.clear()
+	love.graphics.setCanvas(canv_walls)
+	love.graphics.clear()
+	
 	love.graphics.translate(-camera.x,-camera.y)
 	
-	for x = 1,level.size.x,1 do
-		for y = 1,level.size.y,1 do
+	for x = 0,level.size.x+1,1 do
+		for y = 0,level.size.y+1,1 do
+			local screenpos = level.GetDrawPos(Vector2(x,y))
 			if level.tilemap[x][y].filled == false then
 				-- draw floor tiles
-				-- walls are done later to ensure correct layering
-				local screenpos = level.GetDrawPos(Vector2(x,y))
+				love.graphics.setCanvas(canv_floortile)
 				love.graphics.draw(level.tilemap[x][y].tileset.tile,screenpos.x,screenpos.y)
+				
+				else
+				-- draw wall tiles on separate layer to ensure correct layering
+				love.graphics.setCanvas(canv_walls)
+				-- walls need to know the states of their neighbors to draw correctly
+				
+				-- first draw NW as it needs to go below this tile's SE
+				-- these are drawn at 70% opacity
+				love.graphics.setColor(1.0,1.0,1.0,0.7)
+				if level.tilemap[x-1][y].filled == false then
+					if level.tilemap[x][y-1].filled == false then
+						-- NW
+						love.graphics.draw(level.tilemap[x][y].tileset.nw,screenpos.x,screenpos.y)
+						else
+						-- W
+						love.graphics.draw(level.tilemap[x][y].tileset.w,screenpos.x,screenpos.y)
+						end
+					else
+					if level.tilemap[x][y-1].filled == false then
+						-- N
+						love.graphics.draw(level.tilemap[x][y].tileset.n,screenpos.x,screenpos.y)
+						end
+					end
+				
+				-- then draw SE
+				love.graphics.setColor(1.0,1.0,1.0,1.0)
+				if level.tilemap[x+1][y].filled == false then
+					if level.tilemap[x][y+1].filled == false then
+						-- SE
+						love.graphics.draw(level.tilemap[x][y].tileset.se,screenpos.x,screenpos.y)
+						else
+						-- E
+						love.graphics.draw(level.tilemap[x][y].tileset.e,screenpos.x,screenpos.y)
+						end
+					else
+					if level.tilemap[x][y+1].filled == false then
+						-- S
+						love.graphics.draw(level.tilemap[x][y].tileset.s,screenpos.x,screenpos.y)
+						end
+					end
+				-- end per-tile draw loop
 				end
 			end
 		end
+	-- end tilemap iteration
+	love.graphics.setCanvas()
+	love.graphics.draw(canv_floortile,0,0,0,2)
+	love.graphics.draw(canv_walls,0,0,0,2)
 	end
 
 -- fill and unfill tiles on the levelmap
 function level.SetTile(tpos,fill,tset)
 	if getmetatable(tpos) ~= Vector2Meta then
-		error("UnfillTile tpos must be a Vector2")
+		error("SetTile tpos must be a Vector2")
 		return
 		end
 	if tset == nil then tset = level.tilemap[tpos.x][tpos.y].tileset end
