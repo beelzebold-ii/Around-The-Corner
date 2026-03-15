3185862205734ce3ff0eb8e7b2892462ccd19794
beelzebold-ii <futurefast125@gmail.com>
2026-03-15 05:41:16 -0500

    slight graphical changes to make the insides of walls less noisy

--- lua/assets.lua
+++ lua/assets.lua
@@ -11,10 +11,12 @@ tile = {
 	wire_wall_e 	= love.graphics.newImage("graphics/tilesets/wire_wall_e.png"),
 	wire_wall_se 	= love.graphics.newImage("graphics/tilesets/wire_wall_se.png"),
 	
+	ruin_tile 		= love.graphics.newImage("graphics/tilesets/ruin_tile.png"),
+	
 	white_tile 		= love.graphics.newImage("graphics/tilesets/white_tile.png"),
 	white_wall_s 	= love.graphics.newImage("graphics/tilesets/white_wall_s.png"),
 	white_wall_e 	= love.graphics.newImage("graphics/tilesets/white_wall_e.png"),
-	white_wall_se 	= love.graphics.newImage("graphics/tilesets/white_wall_se.png")
+	white_wall_se 	= love.graphics.newImage("graphics/tilesets/white_wall_se.png"),
 }
 
 -- tilesets
@@ -36,5 +38,20 @@ tileset = {
 		n		= tile.wall_n,
 		w		= tile.wall_w,
 		nw		= tile.wall_nw
-	}
+	},
+	ruin = {
+		tile 	= tile.ruin_tile,
+		s 		= tile.wire_wall_s,
+		e 		= tile.wire_wall_e,
+		se 		= tile.wire_wall_se,
+		n		= tile.wall_n,
+		w		= tile.wall_w,
+		nw		= tile.wall_nw
+	},
+}
+
+-- other graphics
+graphic = {
+	sel_floor 	= love.graphics.newImage("graphics/sel_tile.png"),
+	sel_full 	= love.graphics.newImage("graphics/sel_full.png"),
 }
